252899944a0fa42d44f3d849798398674711bd29
yjc95 <yujiacheng10@gmail.com>
2022-05-04 01:31:08 +1000

    Twitter Crawle: Client get tweets

--- Twitter_Crawler.py
+++ Twitter_Crawler.py
@@ -2,6 +2,7 @@ import tweepy
 import time
 import pandas
 
+
 class CrawlerConfig:
     API_Key = 'CaOARF3tCzbfb9i85bYNpG2Ac'
     API_Key_Secret = 'KuXILNgkz4im52FE7dweV2xn3LwtjtQCFGvup2dNVmLKiomQ5W'
@@ -12,20 +13,20 @@ class CrawlerConfig:
 
 
 
-
 auth = CrawlerConfig()
 
 start_time = time.time()
 
-client =tweepy.Client(bearer_token=auth.Bearer_Token,
-                      consumer_key= auth.API_Key,
-                      consumer_secret=auth.API_Key_Secret,
-                      access_token=auth.Access_Token,
-                      access_token_secret= auth.Access_Token_Secret,
-                      return_type=dict).get_tweets('80080680482123777',tweet_fields=["text"])
-
+client = tweepy.Client(bearer_token=auth.Bearer_Token,
+                       consumer_key=auth.API_Key,
+                       consumer_secret=auth.API_Key_Secret,
+                       access_token=auth.Access_Token,
+                       access_token_secret=auth.Access_Token_Secret,
+                       return_type=dict).get_tweets('80080680482123777',
+                                                    tweet_fields=['text','created_at'],
+                                                    user_fields=['public_metrics','description','create_at'],
+                                                    expansions=['author_id'])
 
 
 
 print(client)
-
