01572f4e0f89501bb0e6fffcea65636072ac6b5a
Stefan-Zhou <tyzhou97@outlook.com>
2022-05-04 05:12:16 +1000

    Update Twitter Crawler

--- Twitter_Crawler.py
+++ Twitter_Crawler.py
@@ -1,3 +1,4 @@
+import json
 import tweepy
 import time
 import pandas
@@ -11,22 +12,46 @@ class CrawlerConfig:
     Bearer_Token = 'AAAAAAAAAAAAAAAAAAAAAB2ucAEAAAAAhabe8%2F6r9PTdnfb506VKqQGSjS0%3DsEjcDzbkcrQCFxYqsTNf2IzJfAwlu4yK0xvkj2pDDPsJC4WznE'
 
 
+def save(dict, file_path):
+    if isinstance(dict, str):
+        dict = eval(dict)
+    with open(file_path, 'a', encoding='utf-8') as f:
+        str_ = json.dumps(dict, ensure_ascii=False)
+        f.write(str_)
+        f.write('\n')
+
+
+def load(file_path):
+    with open(file_path, 'r', encoding='utf-8') as f:
+        data = f.readline().strip()
+        return json.loads(data)
 
 
 auth = CrawlerConfig()
 
-start_time = time.time()
+# start_time = time.time()
 
 client = tweepy.Client(bearer_token=auth.Bearer_Token,
                        consumer_key=auth.API_Key,
                        consumer_secret=auth.API_Key_Secret,
                        access_token=auth.Access_Token,
                        access_token_secret=auth.Access_Token_Secret,
-                       return_type=dict).get_tweets('80080680482123777',
-                                                    tweet_fields=['text','created_at'],
-                                                    user_fields=['public_metrics','description','create_at'],
-                                                    expansions=['author_id'])
+                       return_type=dict)
 
+count = 0
 
-
-print(client)
+with open('./project-data/train.data.txt', 'r', encoding='utf-8') as f:
+    for line in f.readlines():
+        ids = line[:-2].split(',')
+        # print(type(ids), ids)
+        tweets = {}
+        for id in ids:
+            tweets[id] = client.get_tweets(ids=id,
+                                           tweet_fields=['text', 'created_at', 'lang', 'geo', 'public_metrics'],
+                                           user_fields=['public_metrics', 'description', 'created_at', 'location'],
+                                           expansions=['author_id'])
+            count += 1
+            if count == 450:
+                time.sleep(900)
+        # print(type(tweets), tweets)
+        save(tweets, './project-data/tweet-train.txt')
